efe6e18f87c514e5334ca694a98938d6385de6dd
Tomasz Ignaszewski <tomasz.ignaszewski@also.com>
2025-04-10 13:56:33 +0200

    initial create

---
+++ ITD.Result/Errors/PredefinedError.cs
@@ -0,0 +1,30 @@
+﻿namespace ITD.Result.Errors;
+public abstract record PredefinedError
+{
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.Failure);
+
+    public static readonly Error NullValue = new(
+        "General.Null",
+        "Null value was provided",
+        ErrorType.Failure);
+
+    public static Error InvalidInput(string message = "invalid input provided") => new(
+        "General.InvalidInput",
+        message,
+        ErrorType.Failure);
+
+    public static Error NotFound(string message = "not found") => new(
+        "General.NotFound",
+        message,
+        ErrorType.NotFound);
+
+    public static Error Unauthorized(string message = "Unauthorized") => new(
+        "General.Unauthorized",
+        message,
+        ErrorType.Failure);
+
+    public static Error ValidationError(string code = "General.ValidationError", string message = "Validation error occured") => new(
+        code,
+        message,
+        ErrorType.Validation);
+}
---
+++ ITD.Result/Result.cs
@@ -0,0 +1,82 @@
+﻿using ITD.Result.Errors;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ITD.Result;
+public class Result
+{
+    public Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None ||
+            !isSuccess && error == Error.None)
+        {
+            throw new ArgumentException("Invalid error", nameof(error));
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+        Errors = Enumerable.Empty<Error>();
+    }
+
+    public Result(bool isSuccess, IEnumerable<Error> errors)
+    {
+        if (isSuccess && errors.Any(e => e != Error.None) ||
+            !isSuccess && errors.All(e => e == Error.None))
+        {
+            throw new ArgumentException("Invalid error", nameof(errors));
+        }
+        IsSuccess = isSuccess;
+        Errors = errors;
+        Error = Error.None;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+    public IEnumerable<Error> Errors { get; }
+
+    public static Result Success() => new(true, Error.None);
+
+    public static Result<TValue> Success<TValue>(TValue value) =>
+        new(value, true, Error.None);
+
+    public static Result Failure(Error error) => new(false, error);
+
+    public static Result<TValue> Failure<TValue>(Error error) =>
+        new(default, false, error);
+
+    public static Result<TValue> ValidationFailure<TValue>(Error error) =>
+        new(default, false, error);
+
+    public static Result<TValue> ValidationFailure<TValue>(IEnumerable<Error> errors) =>
+        new(default, false, errors);
+
+    public static Result ValidationFailure(IEnumerable<Error> errors) =>
+        new(false, errors);
+}
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    public Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    public Result(TValue? value, bool isSuccess, IEnumerable<Error> errors)
+        : base(isSuccess, errors)
+    {
+        _value = value;
+    }
+
+    [NotNull]
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can't be accessed.");
+
+    public static implicit operator Result<TValue>(TValue? value) =>
+        value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+}
